035316be06c91b5a5934b4d5dfc1c7195fba74ac
Berkay Sazak <berkaysazak313@gmail.com>
2022-08-30 17:36:04 +0300

    update

--- app/src/main/java/com/example/mycountryapp/model/Model.kt
+++ app/src/main/java/com/example/mycountryapp/model/Model.kt
@@ -1,17 +1,30 @@
 package com.example.mycountryapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-
+@Entity
 data class Country(
+    @ColumnInfo(name = "name")
     @SerializedName("name")
     val countryName: String?,
+    @ColumnInfo(name = "region")
     @SerializedName("region")
     val countryRegion: String?,
+    @ColumnInfo(name = "capital")
     @SerializedName("capital")
     val countryCapital: String?,
+    @ColumnInfo(name = "currency")
     @SerializedName("currency")
     val countryCurrency: String?,
+    @ColumnInfo(name = "language")
     @SerializedName("language")
     val countryLanguage: String?,
+    @ColumnInfo(name = "flag")
     @SerializedName("flag")
-    val imageUrl: String?)
+    val imageUrl: String?){
+    @PrimaryKey(autoGenerate = true)
+    val uuid: Int = 0
+
+}
