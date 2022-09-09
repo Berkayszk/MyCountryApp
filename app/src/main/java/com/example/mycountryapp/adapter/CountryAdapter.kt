6e57d4cbee1446e7b87de7907fea053403c34559
Berkay Sazak <berkaysazak313@gmail.com>
2022-09-09 15:23:30 +0300

    DataBinding

--- app/src/main/java/com/example/mycountryapp/adapter/CountryAdapter.kt
+++ app/src/main/java/com/example/mycountryapp/adapter/CountryAdapter.kt
@@ -1,28 +1,27 @@
 package com.example.mycountryapp.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.databinding.DataBindingUtil
+
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycountryapp.R
+import com.example.mycountryapp.databinding.ItemCountryBinding
 import com.example.mycountryapp.model.Country
-import com.example.mycountryapp.util.downloadFromUrl
-import com.example.mycountryapp.util.placeholderProgressBar
-import com.example.mycountryapp.view.FeedFragmentDirections
-import kotlinx.android.synthetic.main.item_country.view.*
+
 
 class CountryAdapter(val countryList: ArrayList<Country>): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
 
 
-    class CountryViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    class CountryViewHolder(var view: ItemCountryBinding) : RecyclerView.ViewHolder(view.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_country,parent,false)
+        //val view = inflater.inflate(R.layout.item_country,parent,false)
+        val view = DataBindingUtil.inflate<ItemCountryBinding>(inflater,R.layout.item_country,parent,false)
         return CountryViewHolder(view)
 
     }
@@ -32,6 +31,11 @@ class CountryAdapter(val countryList: ArrayList<Country>): RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+
+        holder.view.country = countryList[position]
+
+
+        /*
         //item_country
         holder.view.name.text = countryList[position].countryName
         holder.view.region.text = countryList[position].countryRegion
@@ -42,6 +46,8 @@ class CountryAdapter(val countryList: ArrayList<Country>): RecyclerView.Adapter<
             Navigation.findNavController(it).navigate(action)
         }
         holder.view.imageView.downloadFromUrl(countryList[position].imageUrl, placeholderProgressBar(holder.view.context))
+
+         */
 
     }
 
